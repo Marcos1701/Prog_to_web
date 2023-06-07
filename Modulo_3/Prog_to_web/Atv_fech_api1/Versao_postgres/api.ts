e3a07760e7f0ec2e29688c158da27a5539d6da81
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2023-06-07 12:45:57 -0300

    Update api.ts

--- Modulo_3/Prog_to_web/Atv_fech_api1/Versao_postgres/api.ts
+++ Modulo_3/Prog_to_web/Atv_fech_api1/Versao_postgres/api.ts
@@ -1,52 +1,28 @@
+import cors from 'cors';
+
 import express, { Application, Request, Response } from 'express';
-// import router from './router';
+import router from './router.js';
 
-import {
-  insertPostagem, retrievePostagem, retrieveAllPostagens, curtirPostagem,
-  updatePostagem, deletePostagem, retrieveAllComentariostoPostagem, retrieveComentario,
-  insertComentario, updateComentario, deleteComentario
-} from './Banco_de_dados/consultas_bd.js'
+export const app: Application = express();
+app.use(cors({ origin: true }));
 
-const app: Application = express();
 
+app.use(express.json())
+app.use(express.raw({ type: 'application/vnd.custom-type' }));
+app.use(express.text({ type: 'text/html' }));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'));
-app.use(express.json())
-// app.use(router);
+app.use(router);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Bem vindo ao microblog!!')
+    res.send('Bem vindo ao microblog!!')
 });
-
-app.get('/posts', retrieveAllPostagens, (req: Request, res: Response) => {
-  // console.log("cheguei aqui")
-  res.send(res.locals.postagens);
-});
-app.get('/posts/:id', retrievePostagem, (req: Request, res: Response) => {
-  // console.log("cheguei aqui")
-  res.send(res.locals.postagem);
-});
-app.post('/posts', insertPostagem, (req: Request, res: Response) => {
-  // console.log("cheguei aqui")
-  res.send(res.locals.postagem);
-});
-app.put('/posts/:id', updatePostagem);
-app.patch('/posts/:id', updatePostagem);
-app.delete('/posts/:id', deletePostagem);
-app.patch('/posts/:id/like', curtirPostagem);
-app.post('/posts/:id/like', curtirPostagem)
-app.post('/posts/:id/comentarios', insertComentario);
-app.get('/posts/:id/comentarios', retrieveAllComentariostoPostagem);
-app.get('/posts/:id/comentarios/:id_comentario', retrieveComentario);
-app.put('/posts/:id/comentarios/:id_comentario', updateComentario);
-app.delete('/posts/:id/comentarios/:id_comentario', deleteComentario);
-
 
 
 app.use(function (req: Request, res: Response, next: Function) {
-  res.status(404).send('Sorry cant find that!');
+    res.status(404).send('Sorry cant find that!');
 });
 
-app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
-});
+app.listen(3000, () =>
+  console.log(`API available on http://localhost:3000`)
+);
